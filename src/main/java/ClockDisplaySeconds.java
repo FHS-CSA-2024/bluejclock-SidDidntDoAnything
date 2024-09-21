af32a36c62b0e26d87509692dd0a62efa5c0f3e6
SidDidntDoAnything <77165813+SidDidntDoAnything@users.noreply.github.com>
2024-09-21 00:48:48 -0700

    Update ClockDisplaySeconds.java

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,6 +1,58 @@
 
+
 public class ClockDisplaySeconds
 {
-    //Extend ClockDisplaySeconds to include a seconds field.
-    //Update all methods and parameters to accomodate this change
+   
+   
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private NumberDisplay seconds;
+    private String display;
+
+    
+    public ClockDisplaySeconds(){
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
+        updateDisplay();
+    }
+    
+    //same as the other
+    public ClockDisplaySeconds(int hour, int minute, int second){
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
+        setTime(hour, minute, second);
+    }
+    
+    //just calls number display super simple
+    public void timeTick(){
+        seconds.increment();
+        if (seconds.getValue() == 0){
+            minutes.increment();
+        }
+        if (minutes.getValue() == 0){
+            hours.increment();
+        }
+        updateDisplay();
+    }
+    
+    //literally just the same
+    public void setTime(int hour, int minute, int second){
+        hours.setValue(hour);
+        minutes.setValue(minute);
+        seconds.setValue(second);
+        updateDisplay();
+    }
+    
+   //just returns
+    public String getTime(){
+        return display;
+    }
+
+    // now its hh:mm:ss
+    public void updateDisplay(){
+        display = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + ":" + seconds.getDisplayValue();
+    }
 }
+
